019bfcc9f41c45244d95b21fdbaca81a5c3fde9c
dsoraluz <d.soraluz@gmail.com>
2017-02-16 12:26:36 -0500

    new game logic complete, minus a few bugs

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var myGlobalGame;
+var numberOfTries;
 
 $(document).ready(function(){
 
@@ -6,12 +7,20 @@ $(document).ready(function(){
   myGlobalGame = new Battleship();
   myGlobalGame.board = myGlobalGame._randomBoardGenerator();
 
+  numberOfTries = 25;
+
 
   console.log(myGlobalGame.board);
   renderReferenceBoard();
   renderBoard();
   loadSounds();
 
+
+  $('.game-over-modal').hide();
+  $('.winner-modal').hide();
+
+  $('.level-modal').css('visibility', 'visible');
+  $('.level-modal').fadeOut(5000);
 
 
   $('.ship-cell').click(function(e){
@@ -25,49 +34,17 @@ $(document).ready(function(){
     // var colIndex = this.getAttribute('data-col');
     //
     // myGlobalGame._sendHit(rowIndex,colIndex);
-    renderReferenceBoard();
+    //renderReferenceBoard();
 
     // console.log("row = " + rowIndex + " column = " + colIndex);
 
   });
-
-
-  $('.row-entry-box').focus(function(e){
-    $(this).val('');
-  });
-
-// $(".row-entry-box").focusout(function(e){
-//   if ($(this).val('')){
-//     $(this).val('Enter Row');
-//   }
-// });
-
-  $('.column-entry-box').focus(function(e){
-          $(this).val('');
-  });
-// $(".column-entry-box").focusout(function(e){
-//   if ($(this).val('')){
-//     $(this).val('Enter Column');
-//   }
-// });
-
-$('#send-hit').click(function(e){
-  var row = $('.row-entry-box').val();
-  var column = $('.column-entry-box').val();
-
-  myGlobalGame._sendHit(row, column);
-  renderReferenceBoard();
-  $('.cell').click();
-});
-
 });
 
 function renderReferenceBoard(){
 
   $('.reference-container').empty();
   var referenceHtml;
-
-
 
 
   myGlobalGame.referenceBoard.forEach(function(row,rowIndex){
@@ -94,13 +71,92 @@ function renderReferenceBoard(){
     var colIndex = this.getAttribute('data-col');
 
     myGlobalGame._sendHit(rowIndex,colIndex);
+
+    numberOfTries -= 1;
+    $('.tries').html(numberOfTries);
+
+    if (myGlobalGame.hitCounter === 10){
+      newLevel();
+    }
+
+    if(numberOfTries === 0)
+    {
+      $('.game-over-modal').show();
+    }
+
     renderReferenceBoard();
-
-    // console.log("row = " + rowIndex + " column = " + colIndex);
-
+    renderBoard();
   });
 }
 
+function newLevel(){
+  myGlobalGame.hitCounter = 0;
+  myGlobalGame.board = myGlobalGame._randomBoardGenerator();
+  myGlobalGame.referenceBoard = myGlobalGame._resetReferenceBoard();
+
+  if($('.reference-board').hasClass('level-1')){
+    $('.level-number-container').removeClass('level-1-container');
+    $('.level-number-container').toggleClass('level-2-container');
+    $('.level-number').html(2);
+    $('.reference-board').removeClass('level-1');
+    $('.reference-board').toggleClass('level-2');
+    numberOfTries = 20;
+    $('.tries').html(numberOfTries);
+
+    $('#level-modal-number').html(2);
+    $('.level-modal').show();
+    $('.level-modal').fadeOut(5000);
+  }
+  else if($('.reference-board').hasClass('level-2')){
+    $('.level-number-container').removeClass('level-2-container');
+    $('.level-number-container').toggleClass('level-3-container');
+    $('.level-number').html(3);
+    $('.reference-board').removeClass('level-2');
+    $('.reference-board').toggleClass('level-3');
+    numberOfTries = 18;
+    $('.tries').html(numberOfTries);
+
+    $('#level-modal-number').html(3);
+    $('.level-modal').show();
+    $('.level-modal').fadeOut(5000);
+  }
+  else if($('.reference-board').hasClass('level-3')){
+    $('.level-number-container').removeClass('level-3-container');
+    $('.level-number-container').toggleClass('level-4-container');
+    $('.level-number').html(4);
+    $('.reference-board').removeClass('level-3');
+    $('.reference-board').toggleClass('level-4');
+    numberOfTries = 15;
+    $('.tries').html(numberOfTries);
+
+    $('#level-modal-number').html(4);
+    $('.level-modal').show();
+    $('.level-modal').fadeOut(5000);
+
+  }
+  else if($('.reference-board').hasClass('level-4')){
+    $('.level-number-container').removeClass('level-4-container');
+    $('.level-number-container').toggleClass('level-5-container');
+    $('.level-number').html(5);
+    $('.reference-board').removeClass('level-4');
+    $('.reference-board').toggleClass('level-5');
+    numberOfTries = 12;
+    $('.tries').html(numberOfTries);
+
+    $('#level-modal-number').html(5);
+    $('.level-modal').show();
+    $('.level-modal').fadeOut(5000);
+
+  }
+
+  else if($('.reference-board').hasClass('level-5')){
+    $('.winner-modal').show();
+  }
+
+}
+
+//Renders board with reveled tiles that user will pick at
+//Remove is desired.
 function renderBoard(){
 
   $('.ship-container').empty();
